177a7598829eea86af0fc3affe7794fbf9999aa2
Antibiotic <ofsposs@gmail.com>
2023-03-23 21:01:20 +0100

    Update bot_2.py

--- bot_2.py
+++ bot_2.py
@@ -1,77 +1,112 @@
-commands = ['add', 'phone', 'change', 'show all']
+import re
+
+COMMANDS = {
+    hello:"hello",
+    add:"add",
+    phone:"phone",
+    change:"change",
+    show_all:"show all"
+}
+commands = ["hello", 'add', 'phone', 'change', 'show all']
 arguments = {}
 
-def add(name, phone):
-    arguments.update({name:phone})
-    print(f"Added <{name}> with number <{phone}>")
 
-def change(name, phone):
-    try:
-        arguments.update({name:phone})
-    except:
-        print("There is no contact with that name")
-        
-def phone(name):
-    for key, value in arguments.items():
-        if name == key:
-            print(value)
-
-def show_all():
-    print(arguments)
-    
 def input_error(func):
-    def wrapper():
+    def wrapper(*args):
         try:
-            func()
+            if len(args)==1:
+                return func(args[0])
+            elif len(args) == 0:
+                return func()
+            elif len(args) == 2:
+                return func(args[0], args[1])
         except ValueError:
             print("Number is incorrect")
-            func()
         except KeyError:
             print("There is no contact with that name")
-            func()
         except IndexError:
             print("Give me a name and phone please")
-            func()
     return wrapper
 
 
 @input_error
+def hello():
+    print("How can I help you?")
+
+
+@input_error
+def add(*args):
+    name = args[0]
+    number = args[1]
+    arguments.update({name:number})
+    print(f"Added <{name}> : <{number}>")
+
+@input_error
+def change(*args):
+    name = args[0]
+    number = args[1]
+    arguments.update({name:number})
+    print(f"Changed <{name}> : <{number}>")
+
+ 
+@input_error        
+def phone(*args):
+    name = args[0]
+    for key, value in arguments.items():
+        if name == key:
+            print(value)
+
+@input_error
+def show_all():
+    print(arguments)
+    
+    
+def handler(string):
+    string = string.lower()
+    command = ""
+    for item in commands:
+        match = re.findall(item, string)
+        if len(match)>0:
+            command = item
+            break
+        
+    result = []
+    
+    if command == "hello":
+        return hello()
+    
+    elif command == "show all":
+        return show_all()
+    
+    else:
+        active = False
+        for i in string.split():
+            if active:
+                result.append(i)
+            elif i == command:
+                active = True
+                result.append(i)
+        return result
+
+@input_error
 def main():
     while True:
-        user_input = input('Enter command: ').split()
+        user_input = input('Enter command: ')
+        user_input = handler(user_input)
         
-        for i in range(1, len(user_input)):
-            if i+1 < len(user_input):
-                arguments[user_input[i]] = user_input[i+1]
-                print(arguments)
-            elif len(user_input)>2:
-                print("Invalid command")
-        if user_input[0] == 'add':
-            name = user_input[1]
-            number = user_input[2]
-            add(name, number)
-        elif user_input[0] == 'phone':
-            name = user_input[1]
-            phone(name)
-        elif user_input[0] == 'change':
-            name = user_input[1]
-            number = user_input[2]
-            change(name, number)
-        elif user_input[0] == 'hello':
-            print("How can I help you?")
-        elif user_input[0] + " " + user_input[1] == 'show all':
-            show_all()
-        elif user_input[0] + " " + user_input[1] == "Good bye":
+        if "exit" in user_input:
             print("Good bye")
             break
-        elif user_input[0] == "exit" or "close":
-            print("Good bye")
-            break
-        else:
-            print("Invalid command")
+        
+        elif user_input[0] == "add":
+            add([user_input[1], user_input[2]])
+            
+        elif user_input[0] == "change":
+            change([user_input[1], user_input[2]])
+            
+        elif user_input[0] == "phone":
+            phone([user_input[1]])
         
         
 if __name__ == "__main__":
     main()
-
-
